3901b36763717e492d7345f5b841936ae1f5c6f6
jgklingo <jgklingo@byu.edu>
2024-09-26 21:59:45 -0600

    add(merge_hulls and compute_hull)

--- convex_hull.py
+++ convex_hull.py
@@ -13,26 +13,34 @@ def divide_points(points: list[tuple[float, float]]) -> list[list[tuple[float, f
     pass
 
 
-def upper_ct(left_points: list[tuple[float, float]], right_points: list[tuple[float, float]]) -> list[tuple[float, float]]:
+def upper_common_tangent(left_points: list[tuple[float, float]], right_points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the two points that make up the upper common tangent of the points in the form [left hull, right hull]"""
     pass
 
 
-def lower_ct(left_points: list[tuple[float, float]], right_points: list[tuple[float, float]]) -> list[tuple[float, float]]:
+def lower_common_tangent(left_points: list[tuple[float, float]], right_points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the two points that make up the lower common tangent of the points in the form [left hull, right hull]"""
     pass
 
 
-def circle_traverse(points: list[tuple[float, float]], start: tuple[float, float], target: tuple[float, float]) -> list[tuple[float, float]]:
-    """List the points circularly clockwise from the start to the target inclusive"""
+def circle_traverse(points: list[tuple[float, float]], start: tuple[float, float], end: tuple[float, float]) -> list[tuple[float, float]]:
+    """Return list of points circularly clockwise from the start to the end inclusive"""
     pass
 
 
 def merge_hulls(left_points: list[tuple[float, float]], right_points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the merged convex hull of the two provided hulls"""
-    pass
+    upper_ct = upper_common_tangent(left_points, right_points)
+    lower_ct = lower_common_tangent(left_points, right_points)
+    return circle_traverse(left_points, lower_ct[0], upper_ct[0]) + circle_traverse(right_points, upper_ct[1], lower_ct[1])
 
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the subset of provided points that define the convex hull"""
-    return []
+    if len(points) == 2 or len(points) == 3:
+        return base_convex_hull(points)
+    
+    left_points, right_points = divide_points(points)
+    left_hull = compute_hull(left_points)
+    right_hull = compute_hull(right_points)
+    return merge_hulls(left_hull, right_hull)
